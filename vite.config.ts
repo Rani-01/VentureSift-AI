d3ce1cfc5680e2ce58d0ec83231e593767fd89f5
Kota Rani <kotarani09@gmail.com>
2025-09-04 18:31:29 +0530

    Firebase changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
+    
     const env = loadEnv(mode, '.', '');
     return {
+      plugins: [react()],
+      base: './',
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env.API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY),
+        'process.env.VITE_GEMINI_API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY)
       },
       resolve: {
         alias: {
